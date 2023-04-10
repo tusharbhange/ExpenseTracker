319355e00dbb0b4a319ce04f2d04c7a91260bdf1
tusharbhange <tushar_bhange@yahoo.com>
2023-04-10 10:42:52 +0530

    DOM Manipulation

--- demo.js
+++ demo.js
@@ -1,1 +1,23 @@
-console.log('I love coding !!!')
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title=123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// //document.title =123
+// console.log(document.forms);
+// console.log(document.links);
+//console.log(document.images);
+
+
+ var headTitle =document.getElementById('main-header');
+
+// headTitle.textContent = "New Title";
+// headTitle.innerText="Hello !!!"
+
+headTitle.style.borderBottom="solid 3px #000"
+
+
