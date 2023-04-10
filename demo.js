2e3b441a660a197372d207231edaf8019024010f
tusharbhange <tushar_bhange@yahoo.com>
2023-04-10 19:52:07 +0530

    GetElementByTagName is added

--- demo.js
+++ demo.js
@@ -17,12 +17,12 @@
 // headTitle.style.borderBottom="solid 3px #000"
 
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items)
-console.log(items[1]);
-items[1].textContent="Hello 2";
-items[1].style.fontWeight='bold';
-items[1].style.backgroundColor='green';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items)
+// console.log(items[1]);
+// items[1].textContent="Hello 2";
+// items[1].style.fontWeight='bold';
+// items[1].style.backgroundColor='green';
 
 // items.style.backgroundColor='#f4f4f4';
 
@@ -30,3 +30,19 @@ items[1].style.backgroundColor='green';
 // {
 //     items[i].style.backgroundColor="#f4f4f4";
 // }
+
+
+// Get Elements by Tag name
+
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent="Hello 2";
+// li[1].style.fontWeight='bold';
+// li[1].style.backgroundColor='yellow';
+
+// for(var i=0;i<li.length;i++)
+// {
+//     li[i].style.backgroundColor='#f6f4f4';
+// }
+
