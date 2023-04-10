49561c50c85afb5b024e3e2ebb4071ddb1e1a317
tusharbhange <tushar_bhange@yahoo.com>
2023-04-10 10:52:15 +0530

    Green Color added

--- demo.js
+++ demo.js
@@ -10,14 +10,23 @@
 // //document.title =123
 // console.log(document.forms);
 // console.log(document.links);
-//console.log(document.images);
-
-
- var headTitle =document.getElementById('main-header');
-
+// console.log(document.images);
+// var headTitle =document.getElementById('main-header');
 // headTitle.textContent = "New Title";
 // headTitle.innerText="Hello !!!"
+// headTitle.style.borderBottom="solid 3px #000"
 
-headTitle.style.borderBottom="solid 3px #000"
 
+var items = document.getElementsByClassName('list-group-item');
+console.log(items)
+console.log(items[1]);
+items[1].textContent="Hello 2";
+items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='green';
 
+// items.style.backgroundColor='#f4f4f4';
+
+// for(var i=0; i<items.length;i++)
+// {
+//     items[i].style.backgroundColor="#f4f4f4";
+// }
