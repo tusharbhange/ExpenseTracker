b4ea2f763c5249fdac70d682a070b5556d0619e3
tusharbhange <tushar_bhange@yahoo.com>
2023-04-10 20:15:08 +0530

    QuerySelector and  QuerySelectorAll added

--- demo.js
+++ demo.js
@@ -46,3 +46,35 @@
 //     li[i].style.backgroundColor='#f6f4f4';
 // }
 
+//QuerySelector
+
+// var header = document.querySelector('#main-header');
+
+// header.style.borderBottom = 'solid 5px #ccc';
+
+// var input = document.querySelector('input');
+
+// input.value='Hello World!'
+
+// var submit = document.querySelector('input[type="submit"]');
+
+// submit.value="SEND";
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color='red';
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2');
+// secondItem.style.color='green';
+
+// Querry Selector All
+
+var titles = document.querySelectorAll('title');
+
+console.log(titles);
+  
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='green';
+}
