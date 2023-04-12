57fc6956fcc98c6a3bdc6b48a4ee2c7fd75593e3
tusharbhange <tushar_bhange@yahoo.com>
2023-04-12 07:45:33 +0530

    Added Description in main.html

--- app.js
+++ app.js
@@ -15,13 +15,15 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var newDec = document.getElementById('description').value;
 
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newItem));  
+  li.appendChild(document.createTextNode(newDec));
 
   // Create del button element
   var deleteBtn = document.createElement('button');
